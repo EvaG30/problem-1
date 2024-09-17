6e0ff938ab4282c8290566f7ca03d760b4862e56
Eva <st108255@student.spbu.ru>
2024-09-17 13:04:55 +0300

    Change hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,10 +3,11 @@ first test project */
 
 #include <iostream>
 #include <string>
+#include "hello_some.h"
 
 int main(){
    std::string s;
    std::cout << "Hello, world!" << std::endl;
-   
+   hello_some(s);
    return 0;
 }
