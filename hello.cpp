7e99f8bc03d2d1d7aaa7f6feb43cd16079ec6eb7
Eva <st108255@student.spbu.ru>
2024-09-17 12:20:36 +0300

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+/* Glushkova Eva st108255@student.spbu.ru 
+first test project */
+
+#include <iostream>
+#include <string>
+
+int main(){
+   std::string s;
+   std::cout << "Hello, world!" << std::endl;
+   
+   return 0;
+}
