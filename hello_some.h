8976149fc994d6e3451d9bee91a925dc892dccf5
ShadeOfMorr <shcheka.d@gmail.com>
2024-09-23 12:10:41 +0300

    Update hello_some.h

--- hello_some.h
+++ hello_some.h
@@ -3,7 +3,8 @@ first test project */
 
 #ifndef HELLO_SOME_h 
 #define HELLO_SOME_h 
-
+/* У тебя на этот файл code editor не ругается? Просто перед string должен быть префикс
+* std::, да и в файле должен быть соответствующий include */
 void hello_some(string s); 
 
 #endif 
