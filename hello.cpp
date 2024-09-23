e761c12278b25312d6f21eaf7a8804a3205dba24
ShadeOfMorr <shcheka.d@gmail.com>
2024-09-23 12:12:53 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,12 @@
 /* Glushkova Eva st108255@student.spbu.ru 
 first test project */
+/* Этот файл должен называться main.cpp. Файл, где находится main должен всегда называться так */
 
 #include <iostream>
 #include <string>
+/* На самом деле так делать нехорошо, потому что это загрязняет
+ * пространство имен и иногда провоцирует конфликты имет, поэтому 
+ * привыкай просто везде писать std:: */
 using namespace std; 
 
 #include "hello_some.h"
